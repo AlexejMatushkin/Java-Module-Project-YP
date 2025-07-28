ee5658172749399663a6bc848f1fb2511c548ac0
AlexejMatushkin <matushkin.leha2018@gmail.com>
2025-07-28 14:01:14 +0500

    I have made validation checks in the input data and changed the description of the object in Readme.md.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,16 +7,36 @@ public class Main {
 
         // Цикл для ввода данных о 3 машинах.
         for (int i = 0; i < 3; i++) {
-            System.out.println("Введите название машины №" + (i + 1) + ":");
-            String name = scanner.next(); // Считываем название машины.
+            String name;
+            while (true) {
+                System.out.println("Введите название машины №" + (i + 1) + ":");
+                name = scanner.next(); // Считываем название машины.
+
+                // Проверка на корректность названия
+                if (name == null || name.trim().isEmpty()) {
+                    System.out.println("Ошибка: название не может быть пустым!");
+                } else if (name.length() > 20) {
+                    System.out.println("Ошибка название слишком длинное (макс. 20 симлволов)!");
+                } else if (!name.matches("[a-zA-Zа-яА-Я0-9 ]+")) {
+                    System.out.println("Ошибка: название содержит недопустимые символы!");
+                } else {
+                    break; // Название корректное
+                }
+            }
 
             int speed;
             while (true) { // Бесконечный цикл для проверки скорости.
                 System.out.println("Введите скорость машины №" + (i + 1));
-                speed = scanner.nextInt(); // Считываем скорость.
-                if (speed >= 0 && speed <= 250 )
-                    break;
-                System.out.println("Неправильная скорость"); // Сообщение об ошибке.
+                try {
+                    speed = scanner.nextInt(); // Считываем скорость.
+                    if (speed >= 0 && speed <= 250)
+                        break;
+                    System.out.print("Неправильная скорость!");
+                    System.out.print(" Допустимо 0-250."); // Сообщение об ошибке.
+                } catch (Exception e) {
+                    System.out.println("Ошибка: введите целое число!");
+                    scanner.next(); // Очистка буфера
+                }
             }
 
             Car car = new Car(name, speed); // Создаём объект машины.
@@ -24,7 +44,6 @@ public class Main {
         }
 
         System.out.println("Самая быстрая машина: " + race.getRaceLeader()); // Выводим победителя.
-
         // Закрываем Scanner по завершении работы.
         scanner.close();
     }
